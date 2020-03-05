02c11a4cf1e4bd7d8de74ba06cf54829bd6395e4
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-03-05 18:20:58 +1100

    build: config eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
